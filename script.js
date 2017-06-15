9c0b95c48cb924b623ec9337f14e231b2c5675f5
Wojo <matt.wojtkun@gmail.com>
2017-06-15 15:43:05 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// Properties:
+  // Contestants array ========
+    // Contains each contestant from the contestant constructor
+    // max length: 3
+  // Wheel object array ========
+    // 12 objects
+    // key: values
+      // 'points': 'X'
+      // 2-of-12 objects: "bankrupt" and "lose a turn"
+  // Phrase Array
+    // Array of phrases (1 to start, 3 total)
+
+  // Consonants
+  // Vowels
+
+// Functions:
+  // User custructor
+  // Spin wheel
+  // Choose Letter
+
+$(document).ready(function($) {
+
+  var WheelofFortune = {
+    init: function(){
+      console.log("Let's play Wheel of Fortune!");
+    }
+
+  }
+  WheelofFortune.init();
+});
