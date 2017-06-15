43b458ed7308d41f8922b6ffd8bafa0655e5b4a6
Wojo <matt.wojtkun@gmail.com>
2017-06-15 15:56:22 -0400

    built out pseudo code

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 
 // Properties:
-  // Contestants array ========
+  // ======== Contestants Array ========
     // Contains each contestant from the contestant constructor
     // max length: 3
-  // Wheel object array ========
+  // ======== Wheel Object Array ========
+    // Holds objects representing the wheel and pt values
     // 12 objects
     // key: values
       // 'points': 'X'
       // 2-of-12 objects: "bankrupt" and "lose a turn"
-  // Phrase Array
+  // ======== Phrase Array ========
     // Array of phrases (1 to start, 3 total)
 
   // Consonants
   // Vowels
 
 // Functions:
-  // User custructor
-  // Spin wheel
-  // Choose Letter
+  // ======== Contestant Constructor ========
+    // Create Contestants with the following members:
+      // id, name
+      // add Contestant to Contestants Array
+  // ======== Spin Wheel ========
+    // produce random number between 1 and 11
+    // grab the index corresponding to the random number
+    // add point
+  // ======== Buy A Vowel ========
+    // costs X pts
+  // ======== Guess ========
+    // correct guess? Go again
 
 $(document).ready(function($) {
 
