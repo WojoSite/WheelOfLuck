0f2c8f5fcb8de7a88d66a162b3e1046800bec1b3
Wojo <matt.wojtkun@gmail.com>
2017-06-16 17:04:35 -0400

    made pseudo code and README

--- script.js
+++ script.js
@@ -1,3 +1,235 @@
+
+// +++++++++++++++ Methods +++++++++++++++
+  // ======== Start Game ========
+
+    // create round counter property
+    // create current player object
+    
+    // select first player in player array
+      // display name and
+      // listen for spin wheel click
+        // fires Spin Wheel method
+      //
+
+  // ======== Spin Wheel ========
+    // produce random number between 1 and 11
+    // grab the object in the Wheel Object Array whose index corresponds to the random number
+    // place value in currentSpinVal array
+    // display currentSpinVal
+
+  // ======== Buy Vowel ========
+    // Ask Contestant if they would like to buy a vowel
+    // Yes: subtract 1 pt from their total points. else: return
+
+  // ======== Guess and Check ========
+    // Prompt user for a guess
+      // validate that it is indeed a letter
+    // Take value provided from user input, make it lowercase
+    // check to see if the letter is in the Already Guessed Letters Array
+      // If so, alert the user that they already guessed that letter
+      // Fire Guess and Check
+    // check to see if the letter is in the phrase
+      // Yes?
+        // add relevant points from current spin value to the contestant score
+        // Update display to show guessed letter where applicable (Phrase Board/Guessed Letter)
+        // update point total in display
+        // Fire Spin Wheel method
+      // No?
+        // If there are more contestants
+          // return,
+          // else: Fire Spin Wheel
+
+  // ======== Game Over ========
+    // interate through contestant array
+      // if Contestant array.length = 1
+        // if the Contestant's score is > 10
+          // modal text: congrats you win!, Click to play again
+        // else: modal-text: too bad. play again!
+        // modalm pops up with start new game function that reloads the page
+      // else:
+        // gather contestant scores
+        // compare contestant scores
+        // whoever has the highest score wins.
+
+  // ======== Solve ========
+
+  // ======== Clear/New Game ========
+
+$(document).ready(function($) {
+  var WheelofLuck = {
+    // Contain each contestant from the contestant constructor
+    contestantArray: [],
+    // ======== Phrase Object Array ========
+    phraseArray: [
+      {phrase: "Seventh Inning Stretch", hint: "Baseball Break", cateory: "Baseball"},
+      {phrase: "Secretary of State", hint: "Fourth in Line"},
+      {phrase: "Wiener Schnitzel", hint: "A Little Vienna Cut"}
+    ],
+    // ======== Wheel Object Array ======== Holds objects representing wheel pieces and corresponding pt values
+    wheel: [1,1,1,2,2,2,3,3,4,4,5,"lose turn", "bankrupt"],
+    consonants: ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"],
+    vowels: ["a", "e", "i", "o", "u"],
+    guessedLetters: [],
+    currentSpinVal: null,
+    // ======== Initialize App ========
+    init: function(){
+      console.log("Let's play Wheel of Luck!");
+
+        // Fire Add Listeners method
+        WheelofLuck.addListeners();
+    },
+    // ======== Add Listeners ========
+    addListeners: function(){
+      // "Contestant Add" form: on submit fires Contestant Constructor
+      var addBtn = document.getElementById("contestant-add-btn");
+      addBtn.addEventListener("click", WheelofLuck.addContestant);
+
+      // "Start Game" Button: click fires Game Board Creator method,
+      var addBtn = document.getElementById("start-game-btn");
+      addBtn.addEventListener("click", WheelofLuck.zeroContestantCheck);
+
+    },
+    addContestant: function(){
+      // before firing:
+        // If Contestants Array <= 3, tell user that there are already three players, and to click start game (or disable "add" or "submit" button)
+      if (WheelofLuck.contestantArray.length == 3) {
+        alert("You have added the maximum amount of players (3). Load your game!");
+        //make add button disabled, too!
+      } else if ($('#name-input').val() == "") {
+        alert("You must enter a name before adding a player.")
+      }else{
+      var userNameInput = $('#name-input').val();
+      var nameId = Math.floor(Math.random()*100000+1);
+      var nextContestant = new WheelofLuck.Contestant(userNameInput, nameId);
+      // Fire Clear Construtor method
+      WheelofLuck.clearForm();
+      WheelofLuck.displayContestantAmt();
+      }
+    },
+    // display # of contestants added.
+    displayContestantAmt: function(){
+      $('#added-contestants').html("Contestants: " + WheelofLuck.contestantArray.length);
+    },
+     // If the Contestants Array = 0, ask user to enter at least one contestant to play (or disable "add" or "submit" button)
+    zeroContestantCheck: function(){
+      if (WheelofLuck.contestantArray.length == 0){
+        alert("You must enter at least one contestant to play.");
+      } else {
+        WheelofLuck.gameBoardCreator();
+      }
+    },
+    gameBoardCreator: function(){
+      var contestantsContainer = document.getElementById('contestants-container');
+
+      for (var i = 0; i < WheelofLuck.contestantArray.length; i++) {
+        var contestantDiv = document.createElement("div");
+        contestantDiv.className = "contestant";
+        contestantDiv.setAttribute('id', WheelofLuck.contestantArray[i].id);
+
+        var nameDiv = document.createElement("div");
+        nameDiv.className = "contestant-name";
+        var nameP = document.createElement("p");
+        nameP.className = "name";
+        nameP.innerHTML = WheelofLuck.contestantArray[i].name;
+
+        var ptsDiv = document.createElement("div");
+        ptsDiv.className = "contestant-points";
+        var ptsP = document.createElement("p");
+        ptsP.className = "pts";
+        ptsP.innerHTML = WheelofLuck.contestantArray[i].points;
+
+        nameDiv.appendChild(nameP);
+        ptsDiv.appendChild(ptsP);
+        contestantDiv.appendChild(nameDiv);
+        contestantDiv.appendChild(ptsDiv);
+        contestantsContainer.appendChild(contestantDiv);
+      }
+
+      // Randomly select object from Phrase Object Array
+      var randomNumber = Math.floor((Math.random() * 3));
+      $('#phrase-hint').html( WheelofLuck.phraseArray[randomNumber].hint);
+
+      var currentPhrase = WheelofLuck.phraseArray[randomNumber].phrase;
+
+      var PhraseBdCont = document.getElementById('phrase-board-container');
+
+      var breakEl = document.createElement("br");
+
+      for (var i = 0; i < currentPhrase.length; i++) {
+
+        if (currentPhrase[i] == " ") {
+          var phraseBdDiv = document.createElement("div");
+          phraseBdDiv.className = "phrase-space-div";
+          PhraseBdCont.appendChild(phraseBdDiv);
+
+        } else {
+          var phraseBdDiv = document.createElement("div");
+          phraseBdDiv.className = "phrase-letter-div";
+
+          var phraseBdP = document.createElement("p");
+          phraseBdP.className = "letter";
+          phraseBdP.innerHTML = currentPhrase[i];
+
+          phraseBdDiv.appendChild(phraseBdP);
+          PhraseBdCont.appendChild(phraseBdDiv);
+        }
+      }
+      // WheelofLuck.startGame();
+    },
+    startGame: function(){
+      WheelofLuck.doRound();
+    },
+    doRound: function() {
+      console.log("ROund starting!");
+      // For each contestant in Contestant object array:
+        // Fire Turn method
+      var contestants = WheelofLuck.contestantArray;
+      for (var i = 0; i < contestants.length; i++) {
+        WheelofLuck.doTurn(contestants[i]);
+      }
+    },
+    doTurn: function(contestant){
+      console.log(contestant);
+      console.log("Turn starting!");
+      // Fire Spin Wheel method
+      WheelofLuck.spinWheel();
+      // check if spin is lose turn or bankrupt
+      // Fire Buy Vowel method
+      // Fire Guess and Check method
+    },
+    spinWheel: function(){
+      // ======== Spin Wheel ========
+        // produce random number between 1 and 11
+        var spin = Math.floor((Math.random() * 11));
+        for (var i = 0; i < WheelofLuck.wheel.length; i++) {
+          // grab the object in the Wheel Object Array whose index corresponds to the random number, store in the object prop currentSpinVal
+          WheelofLuck.currentSpinVal = WheelofLuck.wheel[spin];
+        }
+        // display value ("current spin: user, result: x")
+        // return the randomly selected object
+
+    },
+    // ======== Contestant Constructor ========
+    Contestant: function(name, id){
+      this.name = name;
+      this.id = id;
+      this.points = 0;
+      WheelofLuck.contestantArray.push(this);
+    },
+
+    // ======== Clear Contestant Constructor ========
+      // clear Contestant Constructor form
+    clearForm: function(){
+      var nameAddForm = document.getElementById("name-input-form");
+      nameAddForm.reset();
+      return false
+    }
+  }
+  WheelofLuck.init();
+});
+
+// ||||||||||| PSEUDO CODE |||||||||||
+
 // +++++++++++++++ Properties +++++++++++++++
   // ======== Contestants Array ========
     // Contains each contestant from the contestant constructor
@@ -43,6 +275,7 @@
       // If the Contestants Array = 0, ask user to enter at least one contestant to play (or disable "add" or "submit" button)
 
   // ======== Game Board Creator ========
+
     // Create contestant divs based on Contestants Object Array
     // Randomly select object from Phrase Object Array
       // Place selected phrase in Current Phrase object
@@ -50,7 +283,7 @@
     // Create Phrase Letter/Guessed divs in Phrase Board Container
     // Hide Start Game Container
     // Show Game Board container
-    // Fire Start Game method
+
 
   // ======== Contestant Constructor ========
     // Constructor that creates Contestants with the following members:
@@ -119,151 +352,3 @@
   // ======== Solve ========
 
   // ======== Clear/New Game ========
-
-$(document).ready(function($) {
-  var WheelofLuck = {
-    // Contain each contestant from the contestant constructor
-    contestantArray: [],
-    // ======== Phrase Object Array ========
-    phraseArray: [
-      {phrase: "Seventh Inning Stretch", hint: "Baseball Break"},
-      {phrase: "Secretary of State", hint: "Fourth in Line"},
-      {phrase: "Wiener Schnitzel", hint: "A Little Vienna Cut"}
-    ],
-    // ======== Initialize App ========
-    init: function(){
-      console.log("Let's play Wheel of Luck!");
-
-        // Fire Add Listeners method
-        WheelofLuck.addListeners();
-    },
-    // ======== Add Listeners ========
-    addListeners: function(){
-      // "Contestant Add" form: on submit fires Contestant Constructor
-      var addBtn = document.getElementById("contestant-add-btn");
-      addBtn.addEventListener("click", WheelofLuck.addContestant);
-
-      // "Start Game" Button: click fires Game Board Creator method,
-      var addBtn = document.getElementById("start-game-btn");
-      addBtn.addEventListener("click", WheelofLuck.zeroContestantCheck);
-
-    },
-    addContestant: function(){
-      // before firing:
-        // If Contestants Array <= 3, tell user that there are already three players, and to click start game (or disable "add" or "submit" button)
-      if (WheelofLuck.contestantArray.length == 3) {
-        alert("You have added the maximum amount of players (3). You can start playing!");
-        //make add button disabled, too!
-      } else if ($('#name-input').val() == "") {
-        alert("You must enter a name before adding a player.")
-      }else{
-      var userNameInput = $('#name-input').val();
-      var nameId = Math.floor(Math.random()*100000+1);
-      var nextContestant = new WheelofLuck.Contestant(userNameInput, nameId);
-      // Fire Clear Construtor method
-      WheelofLuck.clearForm();
-      WheelofLuck.displayContestantAmt();
-      }
-    },
-    // display # of contestants added.
-    displayContestantAmt: function(){
-      $('#added-contestants').html("Contestants: " + WheelofLuck.contestantArray.length);
-    },
-     // If the Contestants Array = 0, ask user to enter at least one contestant to play (or disable "add" or "submit" button)
-    zeroContestantCheck: function(){
-      if (WheelofLuck.contestantArray.length == 0){
-        alert("You must enter at least one contestant to play.");
-      } else {
-        WheelofLuck.gameBoardCreator();
-      }
-    },
-    gameBoardCreator: function(){
-      // eventually: show game board (display: [whatever])
-
-      console.log("gameBoardCreator!");
-      // var txt3 = document.createElement("p");
-      // txt3.innerHTML = "Text.";
-      // $("body").append(txt1, txt2, txt3);
-
-      var contestantsContainer = document.getElementById('contestants-container');
-
-      for (var i = 0; i < WheelofLuck.contestantArray.length; i++) {
-        var contestantDiv = document.createElement("div");
-        contestantDiv.className = "contestant";
-        contestantDiv.setAttribute('id', WheelofLuck.contestantArray[i].id);
-
-        var nameDiv = document.createElement("div");
-        nameDiv.className = "contestant-name";
-        var nameP = document.createElement("p");
-        nameP.className = "name";
-        nameP.innerHTML = WheelofLuck.contestantArray[i].name;
-
-        var ptsDiv = document.createElement("div");
-        ptsDiv.className = "contestant-points";
-        var ptsP = document.createElement("p");
-        ptsP.className = "pts";
-        ptsP.innerHTML = WheelofLuck.contestantArray[i].points;
-
-        nameDiv.appendChild(nameP);
-        ptsDiv.appendChild(ptsP);
-        contestantDiv.appendChild(nameDiv);
-        contestantDiv.appendChild(ptsDiv);
-        contestantsContainer.appendChild(contestantDiv);
-      }
-
-      // Randomly select object from Phrase Object Array
-      var randomNumber = Math.floor((Math.random() * 3));
-      $('#phrase-hint').html('Phrase Hint: ' + WheelofLuck.phraseArray[randomNumber].hint);
-
-      var currentPhrase = WheelofLuck.phraseArray[randomNumber].phrase;
-
-      var PhraseBdCont = document.getElementById('phrase-board-container');
-
-      var breakEl = document.createElement("br");
-
-      for (var i = 0; i < currentPhrase.length; i++) {
-
-        if (currentPhrase[i] == " ") {
-          var phraseBdDiv = document.createElement("div");
-          phraseBdDiv.className = "phrase-space-div";
-          PhraseBdCont.appendChild(phraseBdDiv);
-
-        } else {
-          var phraseBdDiv = document.createElement("div");
-          phraseBdDiv.className = "phrase-letter-div";
-
-          var phraseBdP = document.createElement("p");
-          phraseBdP.className = "letter";
-          phraseBdP.innerHTML = currentPhrase[i];
-
-          phraseBdDiv.appendChild(phraseBdP);
-          PhraseBdCont.appendChild(phraseBdDiv);
-        }
-
-      }
-
-
-    },
-
-      // ======== Contestant Constructor ========
-        // Constructor that creates Contestants with the following members:
-          // id, name, pts (set to zero)
-          // add Contestant to a Contestants Array
-    Contestant: function(name, id){
-      this.name = name;
-      this.id = id;
-      this.points = 0;
-      console.log(this);
-      WheelofLuck.contestantArray.push(this);
-    },
-
-    // ======== Clear Contestant Constructor ========
-      // clear Contestant Constructor form
-    clearForm: function(){
-      var nameAddForm = document.getElementById("name-input-form");
-      nameAddForm.reset();
-      return false
-    }
-  }
-  WheelofLuck.init();
-});
