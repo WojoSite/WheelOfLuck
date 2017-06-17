19b18be45911d68bf7a7a5f80d321fb7da8658b6
Wojo <matt.wojtkun@gmail.com>
2017-06-16 23:54:59 -0400

    started coding pseudo code: organized methods

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 
 // +++++++++++++++ Methods +++++++++++++++
-  // ======== Game Play ========
-    //create a Full Alphabet propterty - Do I need this?
-    // create round counter property
-    // create Current Player property
 
 // ======== Game Play ========
-    // add contestat at index 0 to Current Player property
+    // add contestant at index 0 to Current Player property
     // round++
     // Fire Do Turn
 
@@ -111,6 +107,8 @@ $(document).ready(function($) {
   var WheelofLuck = {
     // Contain each contestant from the contestant constructor
     contestantArray: [],
+    // create Current Player property
+    currentContestant: null,
     // ======== Phrase Object Array ========
     phraseArray: [
       {phrase: "Seventh Inning Stretch", hint: "Baseball Break", cateory: "Baseball"},
@@ -123,6 +121,8 @@ $(document).ready(function($) {
     vowels: ["a", "e", "i", "o", "u"],
     guessedLetters: [],
     currentSpinVal: null,
+    // create round counter property
+    roundCounter: 0,
     // ======== Initialize App ========
     init: function(){
       console.log("Let's play Wheel of Luck!");
@@ -136,12 +136,17 @@ $(document).ready(function($) {
       var addBtn = document.getElementById("contestant-add-btn");
       addBtn.addEventListener("click", WheelofLuck.addContestant);
 
-      // "Start Game" Button: click fires Game Board Creator method,
-      var addBtn = document.getElementById("start-game-btn");
+      // "Load Game" Button: click fires Game Board Creator method,
+      var addBtn = document.getElementById("load-game-btn");
       addBtn.addEventListener("click", WheelofLuck.zeroContestantCheck);
+
+      // Start Game
+      var startBtn = document.getElementById('start-game');
+      startBtn.addEventListener("click", WheelofLuck.gamePlay);
 
     },
     addContestant: function(){
+
       // before firing:
         // If Contestants Array <= 3, tell user that there are already three players, and to click start game (or disable "add" or "submit" button)
       if (WheelofLuck.contestantArray.length == 3) {
@@ -167,10 +172,13 @@ $(document).ready(function($) {
       if (WheelofLuck.contestantArray.length == 0){
         alert("You must enter at least one contestant to play.");
       } else {
+
         WheelofLuck.gameBoardCreator();
       }
     },
     gameBoardCreator: function(){
+      $('#start-game').css('display', 'inline-block');
+      // HIDE THE START GAME CONTAINER
       var contestantsContainer = document.getElementById('contestants-container');
 
       for (var i = 0; i < WheelofLuck.contestantArray.length; i++) {
@@ -226,40 +234,6 @@ $(document).ready(function($) {
           PhraseBdCont.appendChild(phraseBdDiv);
         }
       }
-      // WheelofLuck.startGame();
-    },
-    startGame: function(){
-      WheelofLuck.doRound();
-    },
-    doRound: function() {
-      console.log("ROund starting!");
-      // For each contestant in Contestant object array:
-        // Fire Turn method
-      var contestants = WheelofLuck.contestantArray;
-      for (var i = 0; i < contestants.length; i++) {
-        WheelofLuck.doTurn(contestants[i]);
-      }
-    },
-    doTurn: function(contestant){
-      console.log(contestant);
-      console.log("Turn starting!");
-      // Fire Spin Wheel method
-      WheelofLuck.spinWheel();
-      // check if spin is lose turn or bankrupt
-      // Fire Buy Vowel method
-      // Fire Guess and Check method
-    },
-    spinWheel: function(){
-      // ======== Spin Wheel ========
-        // produce random number between 1 and 11
-        var spin = Math.floor((Math.random() * 11));
-        for (var i = 0; i < WheelofLuck.wheel.length; i++) {
-          // grab the object in the Wheel Object Array whose index corresponds to the random number, store in the object prop currentSpinVal
-          WheelofLuck.currentSpinVal = WheelofLuck.wheel[spin];
-        }
-        // display value ("current spin: user, result: x")
-        // return the randomly selected object
-
     },
     // ======== Contestant Constructor ========
     Contestant: function(name, id){
@@ -268,13 +242,140 @@ $(document).ready(function($) {
       this.points = 0;
       WheelofLuck.contestantArray.push(this);
     },
-
     // ======== Clear Contestant Constructor ========
       // clear Contestant Constructor form
     clearForm: function(){
       var nameAddForm = document.getElementById("name-input-form");
       nameAddForm.reset();
       return false
+    },
+    gamePlay: function(){
+      // add contestant at index 0 to Current Player property
+      WheelofLuck.currentContestant = WheelofLuck.contestantArray[0];
+      WheelofLuck.roundCounter++;
+      WheelofLuck.doTurn();
+
+    },
+    // ======== Do Turn ========
+    doTurn: function(){
+      WheelofLuck.clear();
+      // display name and current round at top of gameplay div
+      $('#current-turn-name').html(WheelofLuck.currentContestant.name);
+      $('#round').html(WheelofLuck.roundCounter)
+      // listen for spin wheel click
+      var spinBtn = document.getElementById('spin-btn');
+      spinBtn.addEventListener("click", WheelofLuck.spinWheel);
+      var vowelSubmit = document.getElementById('vowel-submit');
+      vowelSubmit.addEventListener("click", WheelofLuck.buyVowel);
+        // add listener to Buy a Vowel Click
+          // make button live when enabled (add class)
+      var consonantInput = document.getElementById('consonant-submit');
+      consonantInput.addEventListener("click", WheelofLuck.consonantEntry);
+        // add listener to Consonant entry Click
+          // make button live when enabled (add class)
+    },
+    spinWheel: function(){
+      console.log("Spin wheel fired!");
+        // produce random number between 1 and 11
+
+        // grab the object in the Wheel Object Array whose index corresponds to the random number
+        // place value in currentSpinVal
+        // display currentSpinVal in gameplay div
+          // if currentSpinVal = "lose turn"
+            // alert("Result: Lose Turn! Next!")
+            // fire Next Contestant Handler
+          // else if currentSpinVal = "bankrupt"
+            // alert("Result: Bankrupt! Next!")
+            // update points total of current player in contestant array to 0.
+            // fire Next Contestant Handler
+          // else
+            // display currentSpinVal in gameplay div
+    },
+    buyVowel: function(){
+      console.log("Buy vowel click success");
+      // Listen: Buy a Vowel on click
+      // if buy a vowel is clicked
+        // grab vowel input value, to lowercase
+        // if vowel is in guessed letters array
+          // alert: [X] has aleady been guessed. [Next player in array's turn!]
+          // Fire Next Contestant method
+        // if current player pt value = 0
+          // alert: player cannot buy a vowel
+        //else:
+          // loop through vowel array
+            // match input value
+            // deduct one point from player's total
+          // fire Check Guess Method
+    },
+    consonantEntry: function(){
+      console.log("Consonant Entry!");
+      // Listen: Consonant entry on click
+      // If guess consonant is clicked
+        // grab value from consonant input, to lowercase
+        // for loop through consonants array
+          // if input value not in consonants array
+            //  alert user to please enter a consonant
+        // else if consonant is in guessed letters array
+          // alert: [X] has aleady been guessed. [Next player in array's turn!]
+          // Fire Next Contestant method
+        // else
+          // loop through consonant array
+            // match input value ensure a valid entry was provided
+    },
+    checkGuess: function(){
+    // ======== Check Guess ========
+      // if the value is in the currently displayed phrase
+        // Fire Handle Success method
+      // if the value is not in the currently displayed phrase
+        // fire Handle Fail method
+    },
+    handleSuccess: function() {
+      // ======== Handle Success ========
+        // tell user that there are X letters! and to spin again
+        // show letter in Phrase Board
+        // add letter to Guessed letters array
+        // display guessed letter in Guessed Letter div
+        // loop through current guessed letters
+          // if current guessed letter is in the vowels array
+            // Fire Do Turn method
+          // else
+            // grab how many times the letter appears in the phrase
+            // multiply current spin val by how many times the letter appears in the phrase
+            // add to player's total
+            // Fire Do Turn method
+    },
+    handleFail: function(){
+      // ======== Handle Fail ========
+        // alert user that there are no letters of that type on the board. + [Next Contestant's] turn!
+        // add letter to Guessed letters array
+        // display guessed letter in Guessed Letter div
+        // Fire Next Contestant Handler
+    },
+    nextContestant: function(){
+      // ======== Next Contestant Handler ========
+        // round++
+        // loop through player array
+          // match index to current player property index
+          // contestantNext = iteration+1
+            // if contestantNext >= Contestant Array length
+              // WheelofLuck.currentSong = array[0]
+              // Fire Do Turn method
+            // else:
+              // WheelofLuck.currentSong = array[nextContestant];
+              // Fire Do Turn method
+    }
+    clear: function(){
+      console.log("Clear!");
+        // ======== Clear ========
+          // select the input fields and clear/reset each one
+          var vowelSearchInput = document.getElementById("vowel-input-container");
+          vowelSearchInput.reset();
+          var consonantSearchInput = document.getElementById("consonant-form");
+          consonantSearchInput.reset();
+          return false
+          // set the current spin val to null
+          // set the spin value display to ""
+
     }
   }
   WheelofLuck.init();
@@ -388,19 +489,3 @@ $(document).ready(function($) {
         // If there are more contestants
           // return,
           // else: Fire Spin Wheel
-
-  // ======== Game Over ========
-    // interate through contestant array
-      // if Contestant array.length = 1
-        // if the Contestant's score is > 10
-          // modal text: congrats you win!, Click to play again
-        // else: modal-text: too bad. play again!
-        // modalm pops up with start new game function that reloads the page
-      // else:
-        // gather contestant scores
-        // compare contestant scores
-        // whoever has the highest score wins.
-
-  // ======== Solve ========
-
-  // ======== Clear/New Game ========
